07932ee02bc722bbfd21d3ce5a22bc366cbd2020
AntonyZ89 <antonygabriel156@hotmail.com>
2020-02-14 12:06:47 -0300

    Custom ActiveQuery

---
+++ src/db/ActiveQuery.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\models\db;
+
+use yii\db\ActiveQuery as ActiveQueryBase;
+use yii2mod\helpers\ArrayHelper;
+
+/**
+ * @property mixed $_alias
+ */
+class ActiveQuery extends  ActiveQueryBase
+{
+    public function get_alias()
+    {
+        return ArrayHelper::last($this->getTableNameAndAlias());
+    }
+}
--- src/model/default/query.php
+++ src/model/default/query.php
@@ -35,7 +35,7 @@ echo "<?php\n";
 ?>
 
 namespace <?= $generator->queryNs ?>;
-use common\models\db\ActiveQuery;
+use antonyz89\templates\db\ActiveQuery;
 
 /**
  * This is the ActiveQuery class for [[<?= $modelFullClassName ?>]].
