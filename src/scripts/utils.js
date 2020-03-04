919f7b60b590b00119e26b4762cf16db237c79fd
AntonyZ89 <Antonybiel1@hotmail.com>
2020-03-03 21:58:34 -0300

    update utils.js

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,3 +1,7 @@
+$('document').on('blur', 'input[data-krajee-datecontrol]', function () {
+    $(this).data('datecontrol').validate();
+});
+
 $('input[data-krajee-datecontrol]').inputmask({
     mask: '99/99/9999'
 });
