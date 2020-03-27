09f0aa48b7277fb22df4a0662c88c3f0570d3e3e
AntonyZ89 <Antonybiel1@hotmail.com>
2020-03-27 15:41:34 -0300

    rand() function added

--- src/db/ActiveQuery.php
+++ src/db/ActiveQuery.php
@@ -3,6 +3,7 @@
 namespace antonyz89\templates\db;
 
 use yii\db\ActiveQuery as ActiveQueryBase;
+use yii\db\Expression;
 use yii2mod\helpers\ArrayHelper;
 
 /**
@@ -13,5 +14,13 @@ class ActiveQuery extends  ActiveQueryBase
     public function get_alias()
     {
         return ArrayHelper::last($this->getTableNameAndAlias());
+    }
+
+    /**
+     * @return static
+     */
+    public function rand()
+    {
+        return $this->orderBy(new Expression('RAND()'));
     }
 }
