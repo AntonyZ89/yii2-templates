9ee0eeb2dd6b6b6f82500d55e5f981ccbfdb0f97
AntonyZ89 <Antonybiel1@hotmail.com>
2020-03-30 23:32:44 -0300

    pjax method by form attr

--- src/scripts/pjax.js
+++ src/scripts/pjax.js
@@ -3,8 +3,8 @@ const search = '#' + $('form[data-pjax]').attr('id');
 const pathname = window.location.pathname;
 
 function refresh() {
-    let params = $(this).closest('form').serialize();
-    $.pjax.reload({url: pathname, container, data: params, method: 'POST'});
+    let form = $(this).closest('form');
+    $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
 }
 
 $(search + ' input').on('keyup change', refresh);
