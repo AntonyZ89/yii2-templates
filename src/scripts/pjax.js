65ee590551d8e5e67346d190e460f27ccbc951ad
Antony <antonyz.dev@gmail.com>
2020-05-08 15:13:18 -0300

    event listening from body

--- src/scripts/pjax.js
+++ src/scripts/pjax.js
@@ -1,11 +1,13 @@
 const container = '#' + $('div[data-pjax-container]').attr('id');
 const search = '#' + $('form[data-pjax]').attr('id');
 const pathname = window.location.pathname;
+const body = $('body');
 
 function refresh() {
     let form = $(this).closest('form');
     $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
 }
 
-$(search + ' input').on('keyup change', refresh);
-$(search + ' select').on('change', refresh);
+
+body.on('keyup change', `${search} input`, refresh);
+body.on('change', `${search} select`, refresh);
