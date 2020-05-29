97958b52e37d40c63a3a7afd0a5c3276c0e915d8
Antony <antonyz.dev@gmail.com>
2020-05-29 16:45:44 -0300

    support to multiple pjax

--- src/scripts/pjax.js
+++ src/scripts/pjax.js
@@ -1,22 +1,29 @@
-const container = '#' + $('div[data-pjax-container]').attr('id');
-const search = '#' + $('form[data-pjax]').attr('id');
 const pathname = window.location.pathname;
 const body = $('body');
 
-function refresh() {
-  let form = $(this).closest('form');
-  $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
-}
+$('div[data-pjax-container]').each(function () {
+  const container = '#' + $(this).attr('id');
+
+  $(this).siblings().each(function () {
+    $(this).find('form[data-pjax]').each(function () {
+      const search = '#' + $(this).attr('id');
+
+      function refresh() {
+        let form = $(this).closest('form');
+        $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
+      }
+
+      body.on('keyup change', `${search}:not([pjax-only-on-submit]) input`, refresh);
+      body.on('change', `${search}:not([pjax-only-on-submit]) select`, refresh);
+      body.on('submit', `${search}[pjax-only-on-submit]`, function () {
+        refresh.bind(this)();
+        return false;
+      });
+      body.on('pjax:end', container, () => loadMasks());
+    });
+  })
+});
 
 if ($.pjax) {
   $.pjax.defaults.scrollTo = false;
 }
-
-body.on('keyup change', `${search}:not([pjax-only-on-submit]) input`, refresh);
-body.on('change', `${search}:not([pjax-only-on-submit]) select`, refresh);
-body.on('submit', `${search}[pjax-only-on-submit]`, function () {
-  refresh.bind(this)();
-  return false;
-});
-
-body.on('pjax:end', container, () => loadMasks());
