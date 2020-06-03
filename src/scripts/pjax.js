db6bfeb3e0408b0a1509f1604a8af4120cf33724
Antony <antonyz.dev@gmail.com>
2020-06-02 23:47:25 -0300

    fix for multiple pjax and unique pjax

--- src/scripts/pjax.js
+++ src/scripts/pjax.js
@@ -5,25 +5,30 @@ $('div[data-pjax-container]').each(function () {
   const container = '#' + $(this).attr('id');
 
   $(this).siblings().each(function () {
-    $(this).find('form[data-pjax]').each(function () {
-      const search = '#' + $(this).attr('id');
-
-      function refresh() {
-        let form = $(this).closest('form');
-        $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
-      }
-
-      body.on('keyup change', `${search}:not([pjax-only-on-submit]) input`, refresh);
-      body.on('change', `${search}:not([pjax-only-on-submit]) select`, refresh);
-      body.on('submit', `${search}[pjax-only-on-submit]`, function () {
-        refresh.bind(this)();
-        return false;
-      });
-      body.on('pjax:end', container, () => loadMasks());
-    });
-  })
+    addPjax($(this), container);
+  });
+  addPjax($(this), container);
 });
 
 if ($.pjax) {
   $.pjax.defaults.scrollTo = false;
+}
+
+function addPjax(element, container) {
+  element.find('form[data-pjax]').each(function () {
+    const search = '#' + $(this).attr('id');
+
+    function refresh() {
+      let form = $(this).closest('form');
+      $.pjax.reload({url: pathname, container, data: form.serialize(), method: form.attr('method') || 'get'});
+    }
+
+    body.on('keyup change', `${search}:not([pjax-only-on-submit]) input`, refresh);
+    body.on('change', `${search}:not([pjax-only-on-submit]) select`, refresh);
+    body.on('submit', `${search}[pjax-only-on-submit]`, function () {
+      refresh.bind(this)();
+      return false;
+    });
+    body.on('pjax:end', container, () => loadMasks());
+  });
 }
