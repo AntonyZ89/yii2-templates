a6d1c24b020425132729d1044a9ef92392d7ef71
Antony <antonyz.dev@gmail.com>
2020-09-08 22:34:24 -0300

    alias in some functions

--- src/db/ActiveQuery.php
+++ src/db/ActiveQuery.php
@@ -29,16 +29,65 @@ class ActiveQuery extends  ActiveQueryBase
      */
     public function onCondition($condition, $params = [])
     {
-        $_condition = [];
-
-        foreach ($condition as $column => $value) {
-            $column = str_replace('@alias', $this->_alias, $column);
-            $_condition[$column] = $value;
-        }
-
-        return parent::onCondition($_condition, $params);
+        return parent::onCondition($this->putAlias($condition), $params);
     }
 
-    // TODO auto alias for others queries
+    /**
+     * {@inheritDoc}
+     */
+    public function andFilterWhere(array $params)
+    {
+        return parent::andFilterWhere($this->putAlias($params));
+    }
 
+    /**
+     * {@inheritDoc}
+     */
+    public function where($condition, $params = []) {
+        return parent::where($this->putAlias($condition), $params);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function andWhere($condition, $params = []) {
+        return parent::andWhere($this->putAlias($condition), $params);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function orWhere($condition, $params = []) {
+        return parent::orWhere($this->putAlias($condition), $params);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function filterWhere(array $condition) {
+        return parent::filterWhere($this->putAlias($condition));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function orFilterWhere(array $condition) {
+        return parent::orFilterWhere($this->putAlias($condition));
+    }
+
+    /**
+     * @param array $params
+     * @return array
+     */
+    protected function putAlias($params) {
+        $_params = [];
+
+        foreach ($params as $column => $value) {
+            $column = str_replace('@alias', $this->_alias, $column);
+            $value = str_replace('@alias', $this->_alias, $value);
+            $_params[$column] = $value;
+        }
+
+        return $_params;
+    }
 }
