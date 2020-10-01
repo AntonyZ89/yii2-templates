7dc72974f75e33f7e51c8a4a348112c05c99d0ef
Antony <antonyz.dev@gmail.com>
2020-10-01 18:45:54 -0300

    fix utils.js

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -2,14 +2,12 @@ $('body').on('blur', 'input[data-krajee-datecontrol]', function () {
   $(this).data('datecontrol').validate();
 });
 
-function loadMasks() {
-  $('input[data-krajee-datecontrol]').inputmask({
-    mask: '99/99/9999'
-  });
+(function () {
+    $('input[data-krajee-kvdatepicker]').inputmask({
+        mask: '99/99/9999'
+    });
 
-  $('input[data-krajee-daterangepicker]').inputmask({
-    mask: '99/99/9999 - 99/99/9999'
-  });
-}
-
-loadMasks();
+    $('input[data-krajee-daterangepicker]').inputmask({
+        mask: '99/99/9999 - 99/99/9999'
+    });
+})()
