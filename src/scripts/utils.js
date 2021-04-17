b79b9b505192dfd324ed2714c690a0a55ebb72c1
Antony <antonyz.dev@gmail.com>
2021-04-17 18:54:48 -0300

    update utils.js

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,5 +1,13 @@
+/**
+ * Some Utils
+ *
+ * @type {jQuery}
+ */
+
 const pathname = window.location.pathname;
 const body = $('body');
+const lang = $('html').attr('lang');
+const modal = $('#modal');
 
 function loadMasks() {
     const datepicker = $('input[data-krajee-kvdatepicker]');
@@ -12,5 +20,70 @@ function loadMasks() {
         $(this).data('datecontrol').validate();
     });
 }
+
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getSelectedRows() {
+    return $('.grid-view').yiiGridView('getSelectedRows');
+}
+
+/**
+ * serialize form
+ *
+ * @param form_element {jQuery}
+ * @returns {{}}
+ */
+function parseFormToYii(form_element) {
+    const obj = {};
+    for (let _ of form_element.serializeArray()) {
+        let columns = _.name.match(/\[[^[\]]+]/g);
+        let _key = _.name.replace(/\[.+]/, '');
+
+        if (columns) {
+            if (columns.length > 1) {
+                let column, ref;
+                ref = (_key in obj ? obj[_key] : (obj[_key] = {}));
+                while (columns.length) {
+                    column = removeBrackets(columns.shift());
+                    if (columns.length) {
+                        if (!(column in ref)) {
+                            ref = ref[column] = {};
+                        } else {
+                            ref = ref[column];
+                        }
+                    } else {
+                        ref[column] = _.value;
+                    }
+                }
+            } else {
+                (columns = removeBrackets(columns[0])) && (_key in obj || (obj[_key] = {})) && (obj[_key][columns] = _.value);
+            }
+        } else {
+            obj[_.name] = _.value;
+        }
+    }
+    return obj;
+}
+
+function removeBrackets(str) {
+    return str.replace(/[[\]]/g, '');
+}
+
+/*
+BLOCK SPECIAL CHARACTERS IN INPUT
+
+$('input').on('input', function() {
+    let c = this.selectionStart,
+        r = /[^a-z0-9_]/gi,
+        v = $(this).val();
+    if(r.test(v)) {
+        $(this).val(v.replace(r, ''));
+        c--;
+    }
+    this.setSelectionRange(c, c);
+});
+*/
 
 loadMasks();
