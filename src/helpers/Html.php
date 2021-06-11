e09fdbda13831e05d5ed052ab84ca482eb063e24
Antony <antonyz.dev@gmail.com>
2021-06-11 03:27:33 -0300

    + helper HTML

--- src/helpers/Html.php
+++ src/helpers/Html.php
@@ -10,11 +10,16 @@ class Html extends \yii\helpers\Html
      * @inheritDoc
      * @param string|array $content the content to be enclosed between the start and end tags. It will not be HTML-encoded.
      * If this is coming from end users, you should consider [[encode()]] it to prevent XSS attacks.
+     * @param string|array $options
      */
     public static function tag($name, $content = '', $options = [])
     {
         if (is_array($content)) {
             $content = implode($content);
+        }
+
+        if (is_string($options)) {
+            $options = ['class' => $options];
         }
 
         return parent::tag($name, $content, $options);
