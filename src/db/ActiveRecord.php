8e1e3fcbbd62d6c171b942f414e666a700bf4928
Antony <antonyz.dev@gmail.com>
2021-07-05 22:19:13 -0300

    dynamic asText using __get()

--- src/db/ActiveRecord.php
+++ src/db/ActiveRecord.php
@@ -3,6 +3,7 @@
 
 namespace antonyz89\templates\db;
 
+use yii\base\UnknownPropertyException;
 use yii\db\ActiveRecord as ActiveRecordBase;
 
 /**
@@ -10,4 +11,27 @@ use yii\db\ActiveRecord as ActiveRecordBase;
  * @package antonyz89\templates\db
  *
  */
-class ActiveRecord extends ActiveRecordBase {}
+class ActiveRecord extends ActiveRecordBase
+{
+    /**
+     * @inheritDoc
+     * @throws UnknownPropertyException
+     */
+    public function __get($name)
+    {
+        try {
+            return parent::__get($name);
+        } catch (UnknownPropertyException $e) {
+            if (str_ends_with($name, 'AsText')) {
+                $name = str_replace('AsText', '', $name);
+
+                return $this->$name !== null ? $this->{'list' . ucfirst($name)}()[$this->$name] : null;
+            }
+
+            throw new UnknownPropertyException($e->getMessage(), $e->getCode(), $e);
+
+            // TODO asCurrency
+        }
+    }
+}
+
