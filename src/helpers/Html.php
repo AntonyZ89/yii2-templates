2e5a8c67e78aeab2d4b0a511494238a9357cabb2
Antony <antonyz.dev@gmail.com>
2021-08-06 02:48:14 -0300

    helpers\Html: `icon()`, `a()`, `row()` and `col()` functions added

--- src/helpers/Html.php
+++ src/helpers/Html.php
@@ -3,6 +3,8 @@
 
 namespace antonyz89\templates\helpers;
 
+use yii\helpers\ArrayHelper;
+use yii\helpers\Url;
 
 class Html extends \yii\helpers\Html
 {
@@ -52,5 +54,78 @@ class Html extends \yii\helpers\Html
         }
 
         return static::input('text', $name, $value, $options);
+    }
+
+    /**
+     * @param string $name
+     * @param array|string $options
+     * @return string
+     * @throws \Exception
+     */
+    public static function icon(string $name, $options = []): string
+    {
+        if (is_array($options)) {
+            $options['class'] = ArrayHelper::getValue($options, 'class') . ' fas fa-' . $name;
+        } else {
+            $options = [
+                'class' => $options . ' fas fa-' . $name
+            ];
+        }
+
+        $tag = ArrayHelper::getValue($options, 'tag', 'i');
+        unset($options['tag']);
+
+        return self::tag($tag, null, $options);
+    }
+
+    /**
+     * @inheritDoc
+     * @param array|string $text
+     */
+    public static function a($text, $url = null, $options = [])
+    {
+        if ($url !== null) {
+            if (is_array($options)) {
+                $options['href'] = Url::to($url);
+            } else {
+                $options = [
+                    'class' => $options,
+                    'href' => Url::to($url)
+                ];
+            }
+        }
+
+        return static::tag('a', $text, $options);
+    }
+
+    /**
+     * @param string|array|null $content
+     * @param string|array $options
+     */
+    public static function row($content, $options = [])
+    {
+        if (empty($options)) {
+            $options['class'] = 'row';
+        } elseif (is_string($options)) {
+            $options = ['class' => 'row ' . $options];
+        }
+
+        return static::tag('div', $content, $options);
+    }
+
+    /**
+     * @param string|array|null $content
+     * @param string|array $options
+     */
+    public static function col($content, $options = [])
+    {
+        if (empty($options)) {
+            $options['class'] = 'col-md-12';
+        } elseif (is_string($options)) {
+            $size = $options;
+            $options = ['class' => "col-md-$size"];
+        }
+
+        return static::tag('div', $content, $options);
     }
 }
