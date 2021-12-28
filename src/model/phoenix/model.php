3f0c1baf40deacfcf47c8f341459697421f5535b
AntonyDev <antonyz.dev@gmail.com>
2021-12-28 12:54:57 -0300

    phoenix model template

---
+++ src/model/phoenix/model.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $queryClassName string query class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $properties array list of properties (property => [type, name. comment]) */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+/* @var $relationsClassHints array list of relations (name => relation declaration) */
+
+$haveCreatedAt = in_array('created_at', $tableSchema->columnNames, true);
+$haveUpdatedAt = in_array('updated_at', $tableSchema->columnNames, true);
+
+if ($haveCreatedAt && $haveUpdatedAt) {
+    $timestampBehavior = 'TimestampBehavior::class,';
+} else if($haveCreatedAt) {
+    $timestampBehavior = "[
+                'class' => TimestampBehavior::class,
+                'updatedAtAttribute' => false
+            ],";
+} else if($haveUpdatedAt) {
+    $timestampBehavior = "[
+                'class' => TimestampBehavior::class,
+                'createdAtAttribute' => false
+            ],";
+} else {
+    $timestampBehavior = null;
+}
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->ns ?>;
+
+use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii2tech\ar\softdelete\SoftDeleteBehavior;
+
+/**
+ * This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+ *
+<?php foreach ($properties as $property => $data): ?>
+ * @property <?= $data['type']." $$property"  . ($data['comment'] ? ' ' . strtr($data['comment'], ["\n" => ' ']) : '') . "\n" ?>
+<?php endforeach; ?>
+<?php if (!empty($relations)): ?>
+ *
+<?php foreach ($relations as $name => $relation): ?>
+ * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+<?php endforeach; ?>
+<?php endif; ?>
+ */
+class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?>
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '<?= $generator->generateTableName($tableName) ?>';
+    }
+<?php if ($generator->db !== 'db'): ?>
+
+    /**
+     * @return \yii\db\Connection the database connection used by this AR class.
+     */
+    public static function getDb()
+    {
+        return Yii::$app->get('<?= $generator->db ?>');
+    }
+<?php endif; ?>
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            <?= $timestampBehavior . "\n" ?>
+            [
+                'class' => SoftDeleteBehavior::class,
+                'softDeleteAttributeValues' => [
+                    'deleted_at' => time()
+                ],
+                'replaceRegularDelete' => true, // mutate native `delete()` method
+                'allowDeleteCallback' => static function (self $model) {
+                    // TODO: if don't have any relation, allow hard delete
+
+                    /**
+                     * @example
+                     *
+                     * $establishmentsCount = $model->getEstablishments()->count();
+                     * $garagesCount = $model->getGarages()->count();
+                     * $regionsCount = $model->getRegions()->count();
+                     * $servicesCount = $model->getServices()->count();
+                     *
+                     * return !$establishmentsCount && !$garagesCount && !$regionsCount && !$servicesCount;
+                     */
+                }
+            ],
+        ];
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [<?= empty($rules) ? '' : ("\n            " . implode(",\n            ", $rules) . ",\n        ") ?>];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+<?php foreach ($labels as $name => $label): ?>
+            <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+        ];
+    }
+<?php foreach ($relations as $name => $relation): ?>
+
+    /**
+     * Gets query for [[<?= $name ?>]].
+     *
+     * @return <?= $relationsClassHints[$name] . "\n" ?>
+     */
+    public function get<?= $name ?>()
+    {
+        <?= $relation[0] . "\n" ?>
+    }
+<?php endforeach; ?>
+<?php if ($queryClassName): ?>
+<?php
+    $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
+    echo "\n";
+?>
+    /**
+     * {@inheritdoc}
+     * @return <?= $queryClassFullName ?> the active query used by this AR class.
+     */
+    public static function find()
+    {
+        $query = new <?= $queryClassFullName ?>(get_called_class());
+
+        return $query->notDeleted();
+    }
+<?php endif; ?>
+}
---
+++ src/model/phoenix/query.php
@@ -0,0 +1,136 @@
+<?php
+/**
+ * This is the template for generating the ActiveQuery class.
+ *
+ * @author Antony Gabriel Pereira
+ * @link https://github.com/AntonyZ89
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+/* @var $className string class name */
+/* @var $modelClassName string related model class name */
+
+$modelFullClassName = $modelClassName;
+if ($generator->ns !== $generator->queryNs) {
+    $modelFullClassName = '\\' . $generator->ns . '\\' . $modelFullClassName;
+}
+
+function match_toupper($m) {
+    return ucfirst($m[1]);
+}
+
+function capitalize($str) {
+    $str = preg_replace('/_id$/', '', $str);
+    return preg_replace_callback('/(?:^|_)([a-zA-Z0-9])/', 'match_toupper', $str);
+}
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->queryNs ?>;
+
+use antonyz89\templates\db\ActiveQuery;
+use yii2tech\ar\softdelete\SoftDeleteQueryBehavior;
+
+/**
+ * This is the ActiveQuery class for [[<?= $modelFullClassName ?>]].
+ *
+ * @see <?= $modelFullClassName . "\n" ?>
+ */
+class <?= $className ?> extends ActiveQuery
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'softDelete' => [
+                'class' => SoftDeleteQueryBehavior::class,
+                'deletedCondition' => [
+                    'IS NOT', 'deleted_at', null,
+                ],
+                'notDeletedCondition' => [
+                    'IS', 'deleted_at', null,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return <?= $modelFullClassName ?>[]|array
+     */
+    public function all($db = null)
+    {
+        return parent::all($db);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return <?= $modelFullClassName ?>|array|null
+     */
+    public function one($db = null)
+    {
+        return parent::one($db);
+    }
+<?php foreach ($tableSchema->columns as $name => $values):
+    if (in_array($name, ['created_at', 'updated_at'])) continue;
+
+    $operator = ($name === 'operator' ? '$_operator' : '$operator');
+    switch ($values->phpType):
+        case 'integer': ?>
+<?= "\n" ?>
+    /**
+     * @param integer $<?= $name . "\n" ?>
+     * @param string <?= $operator . "\n" ?>
+     * @return <?= $modelFullClassName ?>Query
+     */
+    public function where<?= capitalize($name) ?>($<?= $name ?>, <?= $operator ?> = '=')
+    {
+        return $this->andWhere([
+            <?= $operator ?>, '@alias.<?= $name ?>', $<?= $name . "\n" ?>
+        ]);
+    }
+<?php break;
+    case 'string':
+        $isDate = in_array($values->dbType, ['date', 'datetime']); ?>
+<?= "\n" ?>
+    /**
+     * @param string $<?= $name . "\n" ?>
+     * @param string <?= $operator . "\n" ?>
+     * @return <?= $modelFullClassName ?>Query
+     */
+    public function where<?= capitalize($name) ?>($<?= $name ?>, <?= $operator ?> = <?= $isDate ? "'='" : "'LIKE'" ?>)
+    {
+        return $this->andWhere([
+            <?= $operator ?>, '@alias.<?= $name ?>', $<?= $name . "\n" ?>
+        ]);
+    }
+    <?php if($isDate): ?>
+<?= "\n" ?>
+    /**
+     * @param string $start_date
+     * @param string $end_date
+     * @return <?= $modelFullClassName ?>Query
+     */
+    public function where<?= capitalize($name) ?>Between($start_date, $end_date)
+    {
+        return $this->andWhere([
+            'BETWEEN', '@alias.<?= $name ?>', $start_date, $end_date
+        ]);
+    }
+    <?php endif; ?>
+<?php break; ?>
+<?php endswitch; ?>
+<?php endforeach; ?>
+
+}
